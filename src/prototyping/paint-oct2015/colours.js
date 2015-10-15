c143c544c3bbbb53d664760b57c97ae5782bc288
Josh Hawcroft <dev@joshhawcroft.org>
2015-10-16 08:47:34 +1000

    setup prototyping environment for paint tools

---
+++ src/prototyping/paint-oct2015/colours.js
@@ -0,0 +1,154 @@
+/*
+CinsImp
+Colours Palette
+
+*********************************************************************************
+Copyright (c) 2009-2015, Joshua Hawcroft
+All rights reserved.
+
+ May all beings have happiness and the cause of happiness.
+ May all beings be free of suffering and the cause of suffering.
+ May all beings rejoice for the supreme happiness which is without suffering.
+ May all beings abide in the great equanimity; free of attachment and delusion.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the product nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+function Colours()
+{
+
+}
+
+Colours.DEFAULT_SET = [
+	[0,0,0],
+	[41,41,41],
+	[84,84,84],
+	[128,128,128],
+	[168,168,168],
+	[212,212,212],
+	[51,0,0],
+	[102,0,0],
+	[153,0,0],
+	[204,0,0],
+	[255,0,0],
+	[255,255,255],
+	[102,51,51],
+	[153,51,51],
+	[204,51,51],
+	[255,51,51],
+	[153,102,102],
+	[204,153,153],
+	[102,51,0],
+	[153,51,0],
+	[204,51,0],
+	[255,102,102],
+	[255,153,153],
+	[255,204,204],
+	[255,51,0],
+	[204,153,51],
+	[255,153,51],
+	[255,153,102],
+	[255,204,153],
+	[255,255,204],
+	[153,102,0],
+	[153,102,51],
+	[204,102,51],
+	[255,102,51],
+	[204,153,102],
+	[255,255,153],
+	[204,102,0],
+	[255,102,0],
+	[255,153,0],
+	[255,204,0],
+	[255,204,51],
+	[255,204,102],
+	[51,51,0],
+	[51,102,0],
+	[51,153,0],
+	[51,204,0],
+	[102,153,102],
+	[102,204,102],
+	[0,51,0],
+	[0,102,0],
+	[0,153,0],
+	[51,102,51],
+	[51,153,51],
+	[153,255,153],
+	[0,51,51],
+	[51,102,102],
+	[51,153,102],
+	[102,204,153],
+	[51,204,51],
+	[51,255,51],
+	[0,102,102],
+	[0,153,153],
+	[51,153,153],
+	[102,255,153],
+	[153,204,153],
+	[204,255,204],
+	[0,0,255],
+	[102,102,255],
+	[0,204,204],
+	[51,204,204],
+	[102,204,204],
+	[153,204,204],
+	[0,0,51],
+	[0,0,102],
+	[0,0,153],
+	[0,0,204],
+	[102,204,255],
+	[153,204,255],
+	[51,51,102],
+	[51,51,153],
+	[51,51,204],
+	[51,51,255],
+	[102,102,153],
+	[102,102,204],
+	[51,0,51],
+	[51,0,102],
+	[102,0,153],
+	[102,0,204],
+	[102,0,255],
+	[102,0,51],
+	[153,0,51],
+	[102,51,153],
+	[102,51,204],
+	[153,51,255],
+	[255,204,255],
+	[102,51,102],
+	[153,102,153],
+	[204,102,204],
+	[153,102,255],
+	[204,153,204],
+	[255,153,255],
+	[153,51,102],
+	[204,0,102],
+	[255,0,153],
+	[255,51,153],
+	[255,102,153],
+	[255,153,204],
+	[255,255,255]
+];
+
+
