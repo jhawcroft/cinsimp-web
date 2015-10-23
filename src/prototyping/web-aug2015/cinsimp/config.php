5d90e01d0ad5869e84cf17526929c01fef94a84b
Josh Hawcroft <dev@joshhawcroft.org>
2015-10-23 11:41:40 +1000

    cleaning up unnecessary prototypes

--- src/prototyping/web-aug2015/cinsimp/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config->database->host = 'localhost';
-$config->database->username = 'jojo8330_apps';
-$config->database->password = 'hMEl=H(O1I*=';
-$config->database->name = 'jojo8330_apps';
-$config->database->prefix = 'ci_';
-
-$config->art->path = '/home/jojo8330/data/cinsimp/art/';
