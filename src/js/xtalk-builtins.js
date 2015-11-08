be301d777661a9b5471886aaffb3a72e202f5be4
Josh Hawcroft <dev@joshhawcroft.org>
2015-11-08 10:37:24 +1000

    notes on previous implementation builtins

--- src/js/xtalk-builtins.js
+++ src/js/xtalk-builtins.js
@@ -148,57 +148,7 @@ Specific to CinsImp Environment
 
 };
 
-/*
 
-Desktop prototype syntax definitions:
-
-{"beep [<times> [times]]","times", (XTECommandImp)&_cmd_beep},
-    
-    {"find [`mode``b`normal|`c`chars|`c`characters|`w`word|`w`words|`s`string|`p`whole] "
-        "<text> [in <field>]","text,mode,field", (XTECommandImp)&_cmd_find},
-    
-    {"sort [[the] cards] of <bkgnd> [`dir``asc`ascending|`des`descending] by <sortKey>",
-        "sortKey<,dir,bkgnd", (XTECommandImp)&_cmd_sort},
-    {"sort [[the] cards] [`dir``asc`ascending|`des`descending] by <sortKey>",
-        "sortKey<,dir", (XTECommandImp)&_cmd_sort},
-    {"sort <bkgnd> [`dir``asc`ascending|`des`descending] by <sortKey>",
-        "sortKey<,dir,bkgnd", (XTECommandImp)&_cmd_sort},
-    
-    {"visual [effect] {`effect``0`cut|`1`dissolve|`2`wipe left|`3`wipe right|`4`wipe up|`5`wipe down} "
-        "[`speed``-2`very slowly|`-2`very slow|`-1`slowly|`-1`slow|`0`normal|`2`very fast|`1`fast] "
-        "[to {`dest``0`card|`1`black|`2`white|`3`grey|`3`gray}]",
-        "effect,speed,dest",
-        (XTECommandImp)&_cmd_visual},
-    
-    // must use this to capture cases where [card] is missing on the end, and prev & next which aren't
-     handled via ordinal referencing //
-    //"go [to] {`which``-2`next|`-1`prev|`-1`previous|`1`first|`2`second|`3`third|`4`forth|`5`fifth|"
-    //"`6`sixth|`7`seventh|`8`eighth|`9`ninth|`10`tenth|`-3`middle|`-4`any|`-5`last} [[`marked``1`marked] card]
-    // "ANY" is missing from this syntax, because in 1.0 we can't be bothered with the machinery that
-    // would be require to make this work; for starters, the go command would have to be implemented
-    // more on the thread side of things, to make use of the xtalk engine to resolve the random() need
-    // all this really means is that you must say go any card, rather than just saying go any.
-    //"
-    {"go [to] {`which``-2`next|`-1`prev|`-1`previous|`1`first|`2`second|`3`third|`4`forth|`5`fifth|"
-        "`6`sixth|`7`seventh|`8`eighth|`9`ninth|`10`tenth|`-3`middle|`-5`last} [[`marked``1`marked] card]",
-        "which,marked",
-        (XTECommandImp)&_cmd_go_which}, // this definition is causing a leak ***
-    {"go [to] <where>", "where", (XTECommandImp)&_cmd_go_where},
-    
-    {"answer file <prompt> [of type <type1> [or <type2> [or <type3>]]]", "prompt,type1,type2,type3", (XTECommandImp)&_cmd_answer_file},
-    {"answer folder <prompt>", "prompt", (XTECommandImp)&_cmd_answer_folder},
-    {"answer <prompt> [with <btn1> [or <btn2> [or <btn3>]]]", "prompt,btn1,btn2,btn3", (XTECommandImp)&_cmd_answer_choice},
-    
-    {"ask file <prompt> [with <defaultFilename>]", "prompt,defaultFilename", (XTECommandImp)&_cmd_ask_file},
-    {"ask [`password``1`password] <prompt> [with <response>]", "prompt,response,password", (XTECommandImp)&_cmd_ask_text},
-    
-    {"open file <filepath>", "filepath", (XTECommandImp)&_cmd_file_open},
-    {"close file <filepath>", "filepath", (XTECommandImp)&_cmd_file_close},
-    {"read line from file <filepath>", "filepath", (XTECommandImp)&_cmd_file_read_line},
-    {"read from file <filepath> [at <charBegin>] {for <charCount> | until {end|eof|<charEnd>}}", "filepath,charBegin,charEnd,charCount", (XTECommandImp)&_cmd_file_read_chars},
-    {"write <text> to file <filepath> [at {end|eof|<charBegin>}]", "filepath,text,charBegin", (XTECommandImp)&_cmd_file_write_chars},
-    
-    */
 
 
 
