f35163ce72e0591c37fdefd32dc1b721e97ff5a5
Josh Hawcroft <dev@joshhawcroft.org>
2015-11-08 14:44:08 +1000

    reading chunks of type "line" and "char"

--- src/js/xtalk-vm-chunk.js
+++ src/js/xtalk-vm-chunk.js
@@ -53,18 +53,121 @@ xtalk.js ?
 
 */
 
-Xtalk.VM.Chunk = function(in_type, in_from, in_to, in_subject)
+Xtalk.VM.TChunk = function(in_type, in_from, in_to, in_subject)
 {
 	this.type = 'Chunk';
+	this.kind = in_type;
 	this.range_from = in_from;
 	this.range_to = in_to;
 	this.subject = in_subject;
+	
+	// **TODO validation of from and to?
 };
+var TChunk = Xtalk.VM.TChunk;
 
 
-Xtalk.VM.Chunk.prototype.resolve = function()
+Xtalk.VM.TChunk.prototype.get_type = function() { return this.type; }
+
+Xtalk.VM.TChunk.prototype.is_readable = function() { return true; }
+
+
+Xtalk.VM.TChunk.prototype.resolve = function()
 {
 	return this;
+}
+
+
+Xtalk.VM.TChunk.prototype.toText = function()
+{
+	return this.toString()._value;
+}
+
+
+TChunk._line_offset = function(in_text, in_line)
+{
+	if (in_line == 1) return 0;
+	var offset = 0;
+	for (var c = 1; c < in_line; c++)
+	{
+		var off = in_text.indexOf("\n", offset);
+		if (off < 0) return -1;
+		offset = off + 1;
+	}
+	return offset;
+}
+
+
+TChunk._word_offset = function(in_text, in_word)
+{
+
+}
+
+
+TChunk._calculate_char_range = function(in_chunk, in_subject)
+{
+	var range = { from: 0, to: 0, length: 0 };
+	
+	
+	
+	switch (in_chunk.kind)
+	{
+	case 'char':
+		range.from = in_chunk.range_from - 1;
+		if (in_chunk.range_to !== null)
+			range.to = in_chunk.range_to - 1;
+		else
+			range.to = range.from;
+		range.length = range.to - range.from + 1;
+		break;
+		
+	case 'line':
+	{
+		//example: answer line 1 of ("apples" & newline & "oranges" & newline & "peaches")
+		in_subject = in_subject.replace("\r", "\n");
+		range.from = TChunk._line_offset(in_subject, in_chunk.range_from);
+		if (range.from < 0) range.from = in_subject.length;
+		if (in_chunk.range_to !== null)
+			range.to = TChunk._line_offset(in_subject, in_chunk.range_to + 1);
+		else
+			range.to = TChunk._line_offset(in_subject, in_chunk.range_from + 1);
+		if (range.to >= 0) range.to -= 2;
+		else range.to = in_subject.length - 1;
+		range.length = range.to - range.from + 1;
+		break;
+	}
+		
+	case 'word':
+	{
+		
+		break;
+	}
+		
+	case 'item':
+	{
+		break;
+	}
+	}
+	
+	if (range.length < 0)
+	{
+		range.length = 0;
+		range.to = range.from - 1;
+	}
+	
+	return range;
+}
+
+
+Xtalk.VM.TChunk.prototype.toString = function()
+{
+	/* first resolve the subject to a string */
+	var subject = this.subject.toString().toValue();
+	
+	/* now access the specific chunk */
+	var range = TChunk._calculate_char_range(this, subject);
+	
+	/* return the specific substring */
+	return new Xtalk.VM.TString(subject.substr(range.from, range.length));
 }
 
 
