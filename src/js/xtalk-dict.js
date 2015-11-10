c6e76436c7056ac43b8d9a58a38b6d4a0d00cd3f
Josh Hawcroft <dev@joshhawcroft.org>
2015-11-11 08:37:27 +1000

    minor cleanup of xtalk dictionary and notes

--- src/js/xtalk-dict.js
+++ src/js/xtalk-dict.js
@@ -35,6 +35,15 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 
+/*
+TODO: include comprehensive description of all registration functions and their 
+corresponding language structures, with rationale for why to use a specific structure
+ahead of another.
+
+*/
+
+
+
 Xtalk.Dict = {
 
 /*****************************************************************************************
@@ -431,6 +440,12 @@ Language Built-in Initalization
 		this.register_property('dateitems', 'ditm', 'locl', '----', Xtalk.Builtins.the_dateitems);
 		
 		this.register_property('timestamp', 'tmsp', 'locl', '----', Xtalk.Builtins.the_timestamp);
+		// consider old HC synonym: seconds, but with a different epoch?
+		
+		// consider old HC: ticks (only length is particularly important, no specific start)
+		
+		// consider adding: month, weekday functions & properties
+		// with short, abbreviated, long and long English versions
 		
 		
 		
@@ -676,251 +691,41 @@ Language Built-in Initalization
 
 /*
 
-Desktop prototype syntax definitions:
+Command Syntax Still TODO
+-------------------------
 
-{"beep [<times> [times]]","times", (XTECommandImp)&_cmd_beep},
-    
-    {"find [`mode``b`normal|`c`chars|`c`characters|`w`word|`w`words|`s`string|`p`whole] "
-        "<text> [in <field>]","text,mode,field", (XTECommandImp)&_cmd_find},
-    
-    {"sort [[the] cards] of <bkgnd> [`dir``asc`ascending|`des`descending] by <sortKey>",
-        "sortKey<,dir,bkgnd", (XTECommandImp)&_cmd_sort},
-    {"sort [[the] cards] [`dir``asc`ascending|`des`descending] by <sortKey>",
-        "sortKey<,dir", (XTECommandImp)&_cmd_sort},
-    {"sort <bkgnd> [`dir``asc`ascending|`des`descending] by <sortKey>",
-        "sortKey<,dir,bkgnd", (XTECommandImp)&_cmd_sort},
-    
-    {"visual [effect] {`effect``0`cut|`1`dissolve|`2`wipe left|`3`wipe right|`4`wipe up|`5`wipe down} "
-        "[`speed``-2`very slowly|`-2`very slow|`-1`slowly|`-1`slow|`0`normal|`2`very fast|`1`fast] "
-        "[to {`dest``0`card|`1`black|`2`white|`3`grey|`3`gray}]",
-        "effect,speed,dest",
-        (XTECommandImp)&_cmd_visual},
-    
-    // must use this to capture cases where [card] is missing on the end, and prev & next which aren't
-     handled via ordinal referencing //
-    //"go [to] {`which``-2`next|`-1`prev|`-1`previous|`1`first|`2`second|`3`third|`4`forth|`5`fifth|"
-    //"`6`sixth|`7`seventh|`8`eighth|`9`ninth|`10`tenth|`-3`middle|`-4`any|`-5`last} [[`marked``1`marked] card]
-    // "ANY" is missing from this syntax, because in 1.0 we can't be bothered with the machinery that
-    // would be require to make this work; for starters, the go command would have to be implemented
-    // more on the thread side of things, to make use of the xtalk engine to resolve the random() need
-    // all this really means is that you must say go any card, rather than just saying go any.
-    //"
-    {"go [to] {`which``-2`next|`-1`prev|`-1`previous|`1`first|`2`second|`3`third|`4`forth|`5`fifth|"
-        "`6`sixth|`7`seventh|`8`eighth|`9`ninth|`10`tenth|`-3`middle|`-5`last} [[`marked``1`marked] card]",
-        "which,marked",
-        (XTECommandImp)&_cmd_go_which}, // this definition is causing a leak ***
-    {"go [to] <where>", "where", (XTECommandImp)&_cmd_go_where},
-    
-    {"answer file <prompt> [of type <type1> [or <type2> [or <type3>]]]", "prompt,type1,type2,type3", (XTECommandImp)&_cmd_answer_file},
-    {"answer folder <prompt>", "prompt", (XTECommandImp)&_cmd_answer_folder},
-    {"answer <prompt> [with <btn1> [or <btn2> [or <btn3>]]]", "prompt,btn1,btn2,btn3", (XTECommandImp)&_cmd_answer_choice},
-    
-    {"ask file <prompt> [with <defaultFilename>]", "prompt,defaultFilename", (XTECommandImp)&_cmd_ask_file},
-    {"ask [`password``1`password] <prompt> [with <response>]", "prompt,response,password", (XTECommandImp)&_cmd_ask_text},
-    
-    {"open file <filepath>", "filepath", (XTECommandImp)&_cmd_file_open},
-    {"close file <filepath>", "filepath", (XTECommandImp)&_cmd_file_close},
-    {"read line from file <filepath>", "filepath", (XTECommandImp)&_cmd_file_read_line},
-    {"read from file <filepath> [at <charBegin>] {for <charCount> | until {end|eof|<charEnd>}}", "filepath,charBegin,charEnd,charCount", (XTECommandImp)&_cmd_file_read_chars},
-    {"write <text> to file <filepath> [at {end|eof|<charBegin>}]", "filepath,text,charBegin", (XTECommandImp)&_cmd_file_write_chars},
-    
-    
-    {
-        "put <source> [{`mode``into`into|`after`after|`before`before} <dest>]",
-        "source,mode,dest",
-        &_xte_cmd_put,
-    },
-    {
-        "set <prop> to <value>",
-        "prop,value",
-        &_xte_cmd_set,
-    },
-    {
-        "get <expr>",
-        "expr",
-        &_xte_cmd_get,
-    },
-    
-    
-     { "abs", 1, &_xte_math_abs },
-    { "sum", -1, &_xte_math_sum },
-    { "average", -1, &_xte_math_average },
-    { "min", -1, &_xte_math_min },
-    { "max", -1, &_xte_math_max },
-    { "atan", 1, &_xte_math_atan },
-    { "cos", 1, &_xte_math_cos },
-    { "sin", 1, &_xte_math_sin },
-    { "tan", 1, &_xte_math_tan },
-    { "sqrt", 1, &_xte_math_sqrt },
-    { "round", 1, &_xte_math_round },
-    { "trunc", 1, &_xte_math_trunc },
-    { "random", 1, &_xte_math_random },
-    
-     {"system", 0, "system", 0, &bi_system, NULL},
-    {"target", 0, "object", 0, &bi_target, NULL},
-    {"result", 0, "value", 0, &bi_the_result, NULL},
-    {"itemDelimiter", 0, "string", 0, &bi_item_delimiter_get, NULL, &bi_item_delimiter_set},
-    {"numberFormat", 0, "string", 0, &bi_number_fmt_get, NULL, &bi_number_fmt_set},
-    
-    
-     "convert <expr> to {`fmtto``-1`timestamp | `-2`dateitems | `0`short date | "
-        "`1`abbr date | `1`abbrev date | `1`abbreviated date | `2`long date |"
-        "`3`short time | `4`long time}",
-        "expr,fmtto",
-        &_xte_cmd_convert_date,
-        
-         { "date", 0, &_xte_fnc_date },
-    { "time", 0, &_xte_fnc_time },
-    { "timestamp", 0, &_xte_fnc_timestamp },
-    { "dateitems", 0, &_xte_fnc_dateitems },
-    { "month", 1, &_xte_fnc_month },
-    { "weekday", 1, &_xte_fnc_weekday },
-    NULL
-};
+	convert <expr> to {`fmtto``-1`timestamp | `-2`dateitems | `0`short date | `1`abbr date | `1`abbrev date | `1`abbreviated date | `2`long date |`3`short time | `4`long time}
+	
+	find [`mode``b`normal|`c`chars|`c`characters|`w`word|`w`words|`s`string|`p`whole] <text> [in <field>]
+	
+	sort [[the] cards] of <bkgnd> [`dir``asc`ascending|`des`descending] by <sortKey>
+	sort [[the] cards] [`dir``asc`ascending|`des`descending] by <sortKey>
+	sort <bkgnd> [`dir``asc`ascending|`des`descending] by <sortKey>
+	
+	mark all [cards]
+	mark <card>
+	mark cards where <expr>
+	mark cards by finding [<find-mode>] <text> [in <field>]
+	unmark cards  (as above)
+	
 
+Possible Future Command Syntax
+------------------------------
 
-struct XTEPropertyDef _xte_builtin_date_properties[] = {
-    {"date", 0, "string", 0, &_xte_prp_date, NULL},
-    {"time", 0, "string", 0, &_xte_prp_time, NULL},
-    {"timestamp", 0, "real", 0, &_xte_prp_timestamp, NULL},
-    {"dateitems", 0, "string", 0, &_xte_prp_dateitems, NULL},
-    
+	answer file <prompt> [of type <type1> [or <type2> [or <type3>]]]
+	answer folder <prompt>
+	ask file <prompt> [with <defaultFilename>]
+	
+These could work with uploads/downloads on the client-side, ie. preparing a file for
+download on "close" and opening a file that was "answer file"'d:
+	open file <filepath>
+	close file <filepath>
+    read line from file <filepath>
+    read from file <filepath> [at <charBegin>] {for <charCount> | until {end|eof|<charEnd>}}
+    write <text> to file <filepath> [at {end|eof|<charBegin>}]
     
    
-
-
-static struct XTEPropertyDef _xte_builtin_int_properties[] = {
-    {"month", 0, "string", 0, &_xte_prp_month, NULL},
-    {"weekday", 0, "string", 0, &_xte_prp_weekday, NULL},
-   
-    NULL,
-};
-
-static struct XTEPropertyDef _xte_builtin_real_properties[] = {
-    {"month", 0, "string", 0, &_xte_prp_month, NULL},
-    {"weekday", 0, "string", 0, &_xte_prp_weekday, NULL},
-   
-    NULL,
-};
-
-static struct XTEPropertyDef _xte_builtin_str_properties[] = {
-    {"month", 0, "string", 0, &_xte_prp_month, NULL},
-    {"weekday", 0, "string", 0, &_xte_prp_weekday, NULL},
-    
-    NULL,
-};
-
-
-
-static struct XTEClassDef _class_integer = {
-    "integer",
-    0,
-    sizeof(_xte_builtin_int_properties) / sizeof(struct XTEPropertyDef),
-    _xte_builtin_int_properties,
-    NULL,
-};
-
-static struct XTEClassDef _class_real = {
-    "real",
-    0,
-    sizeof(_xte_builtin_real_properties) / sizeof(struct XTEPropertyDef),
-    _xte_builtin_real_properties,
-    NULL,
-};
-
-static struct XTEClassDef _class_boolean = {
-    "boolean",
-    0,
-    0,
-    NULL,
-    NULL,
-};
-
-static struct XTEClassDef _class_string = {
-    "string",
-    0,
-    sizeof(_xte_builtin_str_properties) / sizeof(struct XTEPropertyDef),
-    _xte_builtin_str_properties,
-    NULL,
-};
-
-
-
-
-static XTEVariant* _bi_system_version(XTE *in_engine, void *in_context, int in_prop, XTEVariant *in_owner, XTEPropRep in_representation)
-{
-    int major,minor,bugfix;
-    char buffer[64];
-    _xte_platform_sys_version(&major, &minor, &bugfix);
-    switch (in_representation)
-    {
-        case XTE_PROPREP_NORMAL:
-        case XTE_PROPREP_SHORT:
-            sprintf(buffer, "%d.%02d%02d", major, minor, bugfix);
-            return xte_string_create_with_cstring(in_engine, buffer);
-            return xte_real_create(in_engine, atof(buffer));
-        case XTE_PROPREP_ABBREVIATED:
-        {
-            char buffer[64];
-            sprintf(buffer, "%d.%d", major, minor);
-            return xte_string_create_with_cstring(in_engine, buffer);
-        }
-        case XTE_PROPREP_LONG:
-        {
-            char buffer[64];
-            sprintf(buffer, "%d.%d.%d", major, minor, bugfix);
-            return xte_string_create_with_cstring(in_engine, buffer);
-        }
-    }
-}
-
-static XTEVariant* _bi_system_name(XTE *in_engine, void *in_context, int in_prop, XTEVariant *in_owner, XTEPropRep in_representation)
-{
-    return xte_string_create_with_cstring(in_engine, _xte_platform_sys());
-}
-
-static XTEVariant* _bi_system_test(XTE *in_engine, void *in_context, int in_prop, XTEVariant *in_owner, XTEPropRep in_representation)
-{
-    return xte_string_create_with_cstring(in_engine, "OK");
-}
-
-static struct XTEPropertyDef _system_properties[] = {
-    {"version", 0, "real", 0, &_bi_system_version, NULL},
-    {"name", 0, "string", 0, &_bi_system_name, NULL},
-    {"_test_parser", 0, "string", 0, &_bi_system_test, NULL},
-    NULL
-};
-
-static struct XTEClassDef _class_system = {
-    "system",
-    0,
-    sizeof(_system_properties) / sizeof(struct XTEPropertyDef),
-    _system_properties,
-    NULL,
-    
-    
-    static struct XTEPropertyDef _chunk_props[] = {
-    {"number", 0, "integer", 0, &_xte_chunk_count, NULL},
-    NULL,
-};
-
-
-struct XTEClassDef _xte_builtin_class_chunk = {
-    "chunk",
-    0,
-    sizeof(_chunk_props) / sizeof(struct XTEPropertyDef),
-    _chunk_props,
-    &_xte_chunk_read,
-    &_xte_chunk_write,
-    
-    
-     { "characters", "character", "string", 0, &_xte_chunk_chars, NULL, &_xte_chunk_count },
-    { "words", "word", "string", 0, &_xte_chunk_words, NULL, &_xte_chunk_count },
-    { "lines", "line", "string", 0, &_xte_chunk_lines, NULL, &_xte_chunk_count },
-    { "items", "item", "string", 0, &_xte_chunk_items, NULL, &_xte_chunk_count },
-    */
-    
-    
+*/
     
 
 Xtalk.Dict.init_builtins();
